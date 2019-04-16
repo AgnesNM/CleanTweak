9304680ceba98bcc4ccadda7c09106c89d15ae4b
Agnes NM <agnesndutamwaniki@gmail.com>
2019-04-16 16:19:58 +0300

    edit js code to only show result after tthe function has executed

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 //UI Logic
-var business = document.getElementById('brandName').value;
-var published = document.getElementById('pubMonth').value;
-var category = document.getElementById('contentType').value;
-var brand = {
-  "Adtech digital": true,
-	"March 2016":true,
-	image: "Rubber Duck",
-}
+
 
 function collateContent() {
-	if ((business = "Adtech digital") && (published = "March 2016") && (category = "image")) {
-		document.getElementById('output').innerHTML = brand.image;
-	} else {
+  var brand = {
+    "Adtech digital": true,
+  	"March 2016":true,
+  	image: "Rubber Duck",
+  };
+  var business = document.getElementById('brandName').value;
+  var published = document.getElementById('pubMonth').value;
+  var category = document.getElementById('contentType').value;
+	 if ((business === "Adtech digital") && (published === "2016-03") && (category === "image")) {
+		document.getElementById('output').innerHTML = brand["image"];
+	 } else {
 
-	}
-}
-
-//Can I store an image in Javscript? Check this out: https://stackoverflow.com/questions/2395765/store-images-in-javascript-object and read about data islands in Javascript
+	 }
+};
+//Can I store an image in Javascript? Check this out: https://stackoverflow.com/questions/2395765/store-images-in-javascript-object and read about data islands in Javascript
