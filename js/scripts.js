167a957068f9ecb7b66a4fcdfb62835cf553e0c1
Agnes NM <agnesndutamwaniki@gmail.com>
2019-04-19 17:09:50 +0300

    create navbar and add the what is to displayed if there is no content to be displayed for that particular period

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ function collateContent() {
 	 if ((business === "Adtech digital") && (published === "2016-03") && (category === "image")) {
 		document.getElementById('output').innerHTML = brand["image"];
 	 } else {
-
+    document.getElementById('output'). innerHTML = "Sorry, we did not find content for this period."
 	 }
 };
 //Can I store an image in Javascript? Check this out: https://stackoverflow.com/questions/2395765/store-images-in-javascript-object and read about data islands in Javascript
