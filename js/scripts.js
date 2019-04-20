ed7268d81a245e75d0fabd70d1c6847aecf3f183
Agnes NM <agnesndutamwaniki@gmail.com>
2019-04-20 17:47:38 +0300

    connect project to firebase and add footer to the home page

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,23 @@
 
 
 function collateContent() {
-  var brand = {
-    "Adtech digital": true,
-  	"March 2016":true,
-  	image: "Rubber Duck",
-  };
-  var business = document.getElementById('brandName').value;
-  var published = document.getElementById('pubMonth').value;
-  var category = document.getElementById('contentType').value;
+	var brand = {
+		"Adtech digital": true,
+		"March 2016": true,
+		image: "Rubber Duck",
+	};
+	var business = document.getElementById('brandName').value;
+	var published = document.getElementById('pubMonth').value;
+	var category = document.getElementById('contentType').value;
 	 if ((business === "Adtech digital") && (published === "2016-03") && (category === "image")) {
 		document.getElementById('output').innerHTML = brand["image"];
 	 } else {
-    document.getElementById('output'). innerHTML = "Sorry, we did not find content for this period."
-	 }
+		document.getElementById('output').innerHTML = "Sorry, we did not find content for this period."
+	}
+	var postData = {
+		business: "business",
+		published: "published",
+		category: "category",
+	};
+	firebase.database().ref().child('post').push(postData).key;
 };
-//Can I store an image in Javascript? Check this out: https://stackoverflow.com/questions/2395765/store-images-in-javascript-object and read about data islands in Javascript
