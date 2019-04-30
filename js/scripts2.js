66033fdb071426522c7ab8cdeda2b3e5a550dc96
Agnes NM <agnesndutamwaniki@gmail.com>
2019-04-30 21:45:10 +0300

    add loader, and add div to display a message if there are no results

---
+++ js/scripts2.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCehocnWF1IKysSABX48Yv-XLbq6hLc_iQ",
+    authDomain: "clean-tweak.firebaseapp.com",
+    databaseURL: "https://clean-tweak.firebaseio.com",
+    projectId: "clean-tweak",
+    storageBucket: "clean-tweak.appspot.com",
+    messagingSenderId: "515653229667"
+  };
+  firebase.initializeApp(config);
--- js/scripts3.js
+++ js/scripts3.js
@@ -1,14 +1,17 @@
 let getData = document.getElementById('collate');
+let spinner = document.getElementById('loader');
+spinner.style.display="none";
 getData.addEventListener("click",(event) =>inputData());
 
 function inputData (){
+  spinner.style.display="block";
   let business = document.getElementById('brandName').value;
   let published = document.getElementById('pubMonth').value;
   let category = document.getElementById('contentType').value;
   let retrieve = firebase.database().ref('post');
-  retrieve.orderByChild("business").equalTo(business).on("value",function(data){//rewrite this function, start here
+  retrieve.orderByChild("business","published",).equalTo(business).on("value",function(data){//rewrite this function, start here
     console.log(data.val());
-    if (data.exists()) {
+    if (data.val()!=null) {
       var content = "";
       data.forEach(function(data){
         var validate = data.val();
@@ -22,9 +25,14 @@ function inputData (){
             </div>
         `;
       });
-      document.getElementById('output').innerHTML = content;
+      
     } else {
-
+      content =`<div class="streak">
+        <img src="https://firebasestorage.googleapis.com/v0/b/clean-tweak.appspot.com/o/gifs%2F200w.webp?alt=media&token=9ed06448-1b65-415e-ab99-7b39de6b7423" alt="">
+        <p>Ooops, we don't seem to have any results for now, sorry!<p>
+      </div>`
     }
+    document.getElementById('output').innerHTML = content;
+    spinner.style.display="none";
   });
 }
