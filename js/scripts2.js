f1ef0ca6eb8e037a753e73e4c6b94ba865b12af5
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-02 15:00:29 +0300

    remove firebase

--- js/scripts2.js
+++
@@ -1,8 +0,0 @@
-var config = {    
-    authDomain: "clean-tweak.firebaseapp.com",
-    databaseURL: "https://clean-tweak.firebaseio.com",
-    projectId: "clean-tweak",
-    storageBucket: "clean-tweak.appspot.com",
-    messagingSenderId: "515653229667"
-  };
-  firebase.initializeApp(config);
