99dac2817c70b6608f1f37703f2af45e43dee94d
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-11 18:31:43 +0300

    edit server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const app = express();
+const app = require('../app/app.js');
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log(`Our app is running on http://localhost: + ${port}`);
 });
