a0a2891e59f0b866ae918d7ff0dcf0f01a077e98
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-11 15:24:18 +0300

    unsaved changes

--- src/server.js
+++ src/server.js
@@ -61,8 +61,6 @@ const app = require('../app');
 
 let port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
-
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
