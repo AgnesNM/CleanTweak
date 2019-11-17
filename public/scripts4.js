e8e51afce548f5d697fbdc3f85a72772aac5605f
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-17 19:53:46 +0300

    display results on the display area

--- public/scripts4.js
+++ public/scripts4.js
@@ -1,4 +1,4 @@
-//GET results from API with query parameter as user input value
+//display results
 
 const apiKey = 'f4964f60-9be0-11e9-97f8-6d145d4cca4d';
 
@@ -7,8 +7,9 @@ const getUserInput = () => {
     return userInputVal;  
 };
 
-document.querySelector('#collate').addEventListener('click', () => {       
-    searchResults(getUserInput());    
+document.querySelector('#collate').addEventListener('click', () => {        
+    const displResults = searchResults(getUserInput());  
+    document.querySelector('#display').textContent = displResults;
 });
 
     
@@ -16,5 +17,6 @@ const searchResults = (userInputVal) => {
     const url  = new URL("https://app.zenserp.com/api/v2/search?"),
         params = {q:userInputVal};
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-    fetch(`${url}&apikey=${apiKey}`).then(response => response.json());       
-};
+    // fetch(`${url}&apikey=${apiKey}`).then(response => response.json()); 
+    return 'Here are the results';
+}
