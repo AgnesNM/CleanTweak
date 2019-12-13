a7b3690e8006479e3558f8dcde9218956a0b8c6f
Agnes NM <agnesndutamwaniki@gmail.com>
2019-12-13 17:54:22 +0300

    displaying data after Promise reslution

--- public/scripts4.js
+++ public/scripts4.js
@@ -1,3 +1,26 @@
+// //display results
+
+// const apiKey = 'f4964f60-9be0-11e9-97f8-6d145d4cca4d';
+
+// const getUserInput = () => {
+//     let userInputVal = document.querySelector('#brandName').value;  
+//     return userInputVal;  
+// };
+
+// document.querySelector('#collate').addEventListener('click', () => {        
+//     const displResults = searchResults(getUserInput());    
+//     document.querySelector('#display').textContent = displResults;    
+// });
+
+    
+// const searchResults = (userInputVal) => {
+//     const url  = new URL("https://app.zenserp.com/api/v2/search?"),
+//         params = {q:userInputVal};
+//     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+//     fetch(`${url}&apikey=${apiKey}`).then(response => response.json());     
+// }
+
+
 //display results
 
 const apiKey = 'f4964f60-9be0-11e9-97f8-6d145d4cca4d';
@@ -8,8 +31,8 @@ const getUserInput = () => {
 };
 
 document.querySelector('#collate').addEventListener('click', () => {        
-    const displResults = searchResults(getUserInput());    
-    document.querySelector('#display').textContent = displResults;    
+    searchResults(getUserInput());        
+        
 });
 
     
@@ -17,5 +40,29 @@ const searchResults = (userInputVal) => {
     const url  = new URL("https://app.zenserp.com/api/v2/search?"),
         params = {q:userInputVal};
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-    fetch(`${url}&apikey=${apiKey}`).then(response => response.json());     
-}
+    let result = fetch(`${url}&apikey=${apiKey}`);   
+       result.then(response => {
+        return response.json();
+    }).then(userInputVal => {        
+         Object.values(userInputVal).forEach(value => {  
+            console.log(value);
+            
+            // = {
+            //     let organic: [,,,,{ title },,,,,]}}) = value; 
+            //     console.log(title);
+            // }               
+           
+        //  document.querySelector('#display').innerHTML = finalResult;   
+        });    
+    });
+};
+
+// const finalData = ({organic}) => {
+//     const [data] = organic;
+//     const{position, videos} = data;
+//     const {title, url,parsed} = videos;
+//     const{source,date,channel} = parsed;
+//     console.log(source,date,channel)
+//     console.log(title,url,parsed);
+//     console.log(position);
+// }
