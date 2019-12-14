c1690908c21d7694f5eaf04c95dce2031400de28
Agnes NM <agnesndutamwaniki@gmail.com>
2019-12-14 05:12:52 +0300

    two ways to display unfiltered results - looping through the json data from the API, or destructuring the json data from the API

--- public/scripts4.js
+++ public/scripts4.js
@@ -21,7 +21,40 @@
 // }
 
 
-//display results
+// //display unfiltered results on console via looping through the json data from the API
+
+// const apiKey = 'f4964f60-9be0-11e9-97f8-6d145d4cca4d';
+
+// const getUserInput = () => {
+//     let userInputVal = document.querySelector('#brandName').value;  
+//     return userInputVal;  
+// };
+
+// document.querySelector('#collate').addEventListener('click', () => {        
+//     searchResults(getUserInput());        
+        
+// });
+
+
+// const searchResults = (userInputVal) => {
+//     const url  = new URL("https://app.zenserp.com/api/v2/search?"),
+//         params = {q:userInputVal};
+//     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+//     let result = fetch(`${url}&apikey=${apiKey}`);   
+//        result.then(response => {
+//         return response.json();
+//     }).then(userInputVal => {        
+//        let finalResult =  Object.values(userInputVal).forEach(value => {  
+//              console.log(value);                              
+//         });  
+//         // document.querySelector('#display').innerHTML = finalResult;  
+//     });
+// }    
+
+
+
+// //display unfiltered results on console via destructuring json data from the API
+
 
 const apiKey = 'f4964f60-9be0-11e9-97f8-6d145d4cca4d';
 
@@ -35,34 +68,27 @@ document.querySelector('#collate').addEventListener('click', () => {
         
 });
 
+const finalData = (organic) => {
+    const {organic: [fifthItem]} = finalData;
+    const { organic: [position, videos]} = finalData;
+    const { organic: [{videos:title, url, parsed}]} = finalData;
+    const { organic: [{parsed: source, date, channel}]} = finalData;  
+    console.log(fifthItem);
+    console.log(position);
+    console.log(videos);
+    console.log(title,url,parsed);
+    console.log(source,date,channel)
     
+}
+   
 const searchResults = (userInputVal) => {
     const url  = new URL("https://app.zenserp.com/api/v2/search?"),
         params = {q:userInputVal};
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
     let result = fetch(`${url}&apikey=${apiKey}`);   
-       result.then(response => {
-        return response.json();
-    }).then(userInputVal => {        
-         Object.values(userInputVal).forEach(value => {  
-            console.log(value);
-            
-            // = {
-            //     let organic: [,,,,{ title },,,,,]}}) = value; 
-            //     console.log(title);
-            // }               
-           
-        //  document.querySelector('#display').innerHTML = finalResult;   
-        });    
-    });
-};
-
-// const finalData = ({organic}) => {
-//     const [data] = organic;
-//     const{position, videos} = data;
-//     const {title, url,parsed} = videos;
-//     const{source,date,channel} = parsed;
-//     console.log(source,date,channel)
-//     console.log(title,url,parsed);
-//     console.log(position);
-// }
+    result.then(response => {
+     return response.json();
+ }).then(finalData => {
+     console.log(finalData)
+ });
+}    
