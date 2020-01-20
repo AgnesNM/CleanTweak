e2b81aefae872e9f2c8a7f17f970b5e491ebbd76
Agnes NM <agnesndutamwaniki@gmail.com>
2020-01-20 18:36:18 +0300

    edit config file

--- public/scripts4.js
+++ public/scripts4.js
@@ -1,7 +1,6 @@
 //displaying a loader as we await results and a gif, if there are no results
 
-
-let apiKey = `${myAPI.key}`;
+let apiKey = myAPI.key;
 
 const getUserInput = () => {	
 		let userInputVal = document.querySelector('#brandName').value;			
